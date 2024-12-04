efac7114cc1cb498a4036f45b401e2faba36bacf
Snowwpanda <pascal.su@empa.ch>
2024-12-04 20:31:36 +0100

    advent24 day4 part2

--- adventofcode24/4/4.py
+++ adventofcode24/4/4.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 
 def count_subarray(arr, search_string):
-    count = 0
     # arr to string
     full_str = ''.join(arr)
     return full_str.count(search_string)
@@ -18,7 +17,7 @@ def solveA(file_name):
         # print(my_file.read())
         lines = my_file.readlines()
 
-    # tranform into numpy matrix:
+    # tranform into numpy matrix for rotatoin:
     lines = np.array([[x for x in line.strip()] for line in lines])
 
     xmas_count = 0
@@ -27,22 +26,14 @@ def solveA(file_name):
         xmas_count += count_subarray(line, 'XMAS')
         xmas_count += count_subarray(line, 'SAMX')
         
-    # create matrix but every line shifted by 1 more to the right
-    shifted_lines = np.array([[' ' for x in range(y)] + list(lines[y]) + [' ' for x in range(len(lines) -y -1)]   for y in range(len(lines))]) 
-    shifted_lines = np.rot90(np.fliplr(shifted_lines))
-    # forward and backward:
-    for line in shifted_lines:
-        xmas_count += count_subarray(line, 'XMAS')
-        xmas_count += count_subarray(line, 'SAMX')
-    
-    # mirror along diagonal:
+    # mirror along diagonal (for vertical):
     lines = np.rot90(np.fliplr(lines))
     # forward and backward:
     for line in lines:
         xmas_count += count_subarray(line, 'XMAS')
         xmas_count += count_subarray(line, 'SAMX')
     
-    # create matrix but every line shifted by 1 more to the right
+    # create matrix but every line shifted by 1 more to the right (for diagonal /)
     shifted_lines = np.array([[' ' for x in range(len(lines) -y -1)] + list(lines[y]) + [' ' for x in range(y)]   for y in range(len(lines))]) 
     shifted_lines = np.rot90(np.fliplr(shifted_lines))
     # forward and backward:
@@ -50,12 +41,32 @@ def solveA(file_name):
         xmas_count += count_subarray(line, 'XMAS')
         xmas_count += count_subarray(line, 'SAMX')
     
+    # create matrix but every line shifted by 1 more to the left (for diagonal \)
+    shifted_lines = np.array([[' ' for x in range(y)] + list(lines[y]) + [' ' for x in range(len(lines) -y -1)]   for y in range(len(lines))]) 
+    shifted_lines = np.rot90(np.fliplr(shifted_lines))
+    # forward and backward:
+    for line in shifted_lines:
+        xmas_count += count_subarray(line, 'XMAS')
+        xmas_count += count_subarray(line, 'SAMX')
 
     res = xmas_count
 
     # Print result
     print("Answer Part 1:")
     print(res)
+
+def transform1(pos, index, n):
+    # turn diagonal coordinates into matrix coordinates
+    x = pos
+    y = index + pos - (n -1)
+    return (x, y)
+
+    
+def transform2(pos, index, n):
+    # turn diagonal coordinates into matrix coordinates
+    x = pos
+    y = index - pos
+    return (x, y)
 
 
 
@@ -69,10 +80,43 @@ def solveB(file_name):
         lines = my_file.readlines()
 
     
+    # tranform into numpy matrix:
+    lines = np.array([[x for x in line.strip()] for line in lines])
+
+    n = len(lines)
+
+    
+    # create matrix but every line shifted by 1 more to the right (for diagonal \)
+    shifted_lines = np.array([[' ' for x in range(len(lines) -y -1)] + list(lines[y]) + [' ' for x in range(y)]   for y in range(len(lines))]) 
+    shifted_lines = np.rot90(np.fliplr(shifted_lines))
+
+    # Keeping track of all the 'A's with MAS or SAM in the vicinity
+    found_mas = set()
+
+    # forward and backward:
+    for index, line in enumerate(shifted_lines):
+        found_mas.update([transform1(match.start() +1, index, n) for match in re.finditer('MAS', ''.join(line))])
+        found_mas.update([transform1(match.start() +1, index, n) for match in re.finditer('SAM', ''.join(line))])
+
+    
+    count_x_mas = 0
+    # create matrix but every line shifted by 1 more to the left (for diagonal /)
+    shifted_lines = np.array([[' ' for x in range(y)] + list(lines[y]) + [' ' for x in range(len(lines) -y -1)]   for y in range(len(lines))]) 
+    shifted_lines = np.rot90(np.fliplr(shifted_lines))
+    # forward and backward:
+    for index, line in enumerate(shifted_lines):
+        for match in [transform2(match.start() +1, index, n) for match in re.finditer('MAS', ''.join(line))] :
+            if match in found_mas:
+                count_x_mas += 1
+        for match in [transform2(match.start() +1, index, n) for match in re.finditer('SAM', ''.join(line))] :
+            if match in found_mas:
+                count_x_mas += 1
+
+    
 
         
         
-    res = safe_records
+    res = count_x_mas
 
     # Print result
     print("Answer Part 2:")
@@ -85,4 +129,4 @@ def solveB(file_name):
 if __name__ == '__main__':
     solveA('input.txt')
 
-    # solveB('input.txt')
+    solveB('input.txt')
